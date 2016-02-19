6e29710baeae3cf895d56d17192023cad2f869f0
Neethuanna Mathew <Neethuanna.Mathew@Eternusdev.local>
2016-02-19 17:47:06 +0530

    Implemented Redirect to AuthServer functionality

--- src/server/server.js
+++ src/server/server.js
@@ -2,40 +2,25 @@ var express = require('express');
 var app = express();
 var path = require('path');
 app.use(express.static(path.join(__dirname, '../client')));
-app.get('/', function (req, res) {
-    res.render('index.html');
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine', 'html');
+app.get('/login', function (req, res) {
+    res.redirect('http://localhost:3001/auth?continue=http://localhost:8001');
+});
+app.get('/logout', function (req, res) {
+    res.redirect('http://localhost:3001/logOut?continue=http://localhost:8001');
+});
+app.get('/dashboard', function (req, res) {
+    //res.render('index.html');
+    app.locals.username =req.query.user;
+    res.send("Welcome "+app.locals.username)
 });
 
-var http = require('http'),
-io = require('socket.io');
-var server = app.listen(8080, function () {
+var server = app.listen(8001, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('SamplpeWorks app listening at http://%s:%s', host, port);
+    console.log('SampleWorks app listening at http://%s:%s', host, port);
 });
+
 server.listen(8080);
-io = io.listen(server);
 
-// Add a connect listener
-io.sockets.on('connection', function(socket)
-{
-  console.log('Auth server connected.');
-
-  // Disconnect listener
-  socket.on('disconnect', function() {
-  console.log('Auth Server disconnected.');
-  });
-});
-////////////////////////////////////
-// Connect to server
-var io = require('socket.io-client');
-var socket = io.connect('http://localhost:8082', {reconnect: true});
-
-console.log('Connecting to server....');
-
-// Add a connect listener
-socket.on('connect', function(socket) { 
-  console.log('Connected! to Auth Server ');
-});
-
-console.log('Connecting to server....');
